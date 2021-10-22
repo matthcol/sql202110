1d68ab17286108f08e339e5766b32810281b2e59
matthcol <matthias.colin@gmail.com>
2021-10-22 10:27:02 +0200

    jointure interne

---
+++ queries_day3.sql
@@ -0,0 +1,69 @@
+-- les titres des films de l'acteur 285
+select title
+from movies where id in (
+	select id_movie 
+	from play
+	where id_actor = 285);
+
+-- les titres des films joués par Alec Baldwin
+select title
+from movies where id in (
+	select id_movie 
+	from play
+	where id_actor = (
+		select id
+		from stars
+		where name like 'Alec Baldwin'
+	));
+
+-- idem avec Steve McQueen
+select title
+from movies where id in (
+	select id_movie 
+	from play
+	where id_actor in (
+		select id
+		from stars
+		where name like 'Steve McQueen'
+	));
+
+-- titres, années des films, role joué, date de naissance d'un acteur :
+--   1. Alec Baldwin
+--   2. Steve McQueen
+select title, year, role, birthdate
+from movies where id in (
+	select id_movie 
+	from play
+	where id_actor = (
+		select id
+		from stars
+		where name like 'Alec Baldwin'
+	));
+
+-- attention à l'ambiguité
+select * 
+from play join stars on id_actor = id 
+where name like 'Alec Baldwin';
+
+-- idem (prefixer par nom de table)
+select * 
+from play join stars on play.id_actor = stars.id 
+where stars.name like 'Alec Baldwin';
+
+-- idem (renommage sans mot clé as)
+select * 
+from play p join stars s on p.id_actor = s.id 
+where s.name like 'Alec Baldwin';
+
+-- idem (renommage avec mot clé as)
+select * 
+from play as p join stars as s on p.id_actor = s.id 
+where s.name like 'Alec Baldwin';
+
+-- idem (inner facultatif)
+select * 
+from play p inner join stars s on p.id_actor = s.id 
+where s.name like 'Alec Baldwin';
+
+
+
