99789d01ddb15b025a4f0ad07bab4d87280010c0
matthcol <matthias.colin@gmail.com>
2021-10-22 17:10:50 +0200

    end stage

--- franchises_data.sql
+++ franchises_data.sql
@@ -3,3 +3,14 @@ insert into franchises (name) values ('Disney');
 insert into franchises (name) values ('Disney');
 
 select * from franchises;
+
+update movies 
+set id_franchise = 1
+where title like 'Star Wars%'
+	and title not like '%Deleted%';
+
+select * 
+from movies m join franchises f on m.id_franchise = f.id
+where title like 'Star Wars%';
+
+	and title not like '%Deleted%';
